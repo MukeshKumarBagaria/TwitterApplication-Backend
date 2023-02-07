8ba7bd125236ea54dd52ac0033f0b5ffc20472fc
mukesh bagaria <mukeshbagaria@mukeshs-MacBook-Air.local>
2023-02-07 23:42:52 +0530

    updated comment schema with ref path to make comment on tweet and comment on comment

--- src/models/comment.js
+++ src/models/comment.js
@@ -4,9 +4,21 @@ const commentSchema= new mongoose.Schema({
         type:String,
         required:true
     },
-    userEmail:{
-        type:String,
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Tweet', 'Comment']
+    },
+    commentable:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'onModel'
+    }
    
 },{timestamps:true})
 
